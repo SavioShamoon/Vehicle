ed74a9ab380e20d0b85ebae133575bfc12b6d213
Savio Shamoon <savio.shamoon@mrjaffesclass.com>
2022-12-02 11:36:35 -0800

    Shamoon

---
+++ Bike.java
@@ -0,0 +1,23 @@
+public class Bike extends Vehicle {
+    private boolean flatTire;
+    
+    public Bike(String name, double speed) {
+           super(name, speed); 
+    }
+    
+    public void ranOverNail() {
+        flatTire = true;
+    }
+    
+    public void fixTire() {
+        flatTire = false;
+    }
+    
+    @Override
+    public void travel(double time, int direction) {
+        if (!flatTire) {
+            super.travel(time, direction);
+        }
+    }
+}
+
